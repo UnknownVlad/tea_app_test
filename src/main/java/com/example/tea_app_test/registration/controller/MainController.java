310e4dc943142f74f015920a9e15705b3b9e1239
jommka <pretty.pelmeshka@,ail.ru>
2023-12-06 23:46:56 +0300

    ошибка Neither BindingResult nor plain target object for bean name 'userDto' available as request attribute

--- src/main/java/com/example/tea_app_test/registration/controller/MainController.java
+++ src/main/java/com/example/tea_app_test/registration/controller/MainController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -59,10 +60,9 @@ public class MainController {
 
 
     @GetMapping("/registration")
-    public String registration() {
+    public String registration(UserDto userDto) {
         return "index";
     }
-
 
     @PostMapping("/registration")
     public String registration(@Valid UserDto userDto, BindingResult bindingResult) throws MessagingException {
