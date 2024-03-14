d83dc8f7e3462d33ebf8454cd8d44dbaf3e908b0
UnknownVlad <vlad.fox2002@mail.ru>
2024-03-14 11:55:38 +0300

    Переписаны названия ролей т.к некоторые слова зарезервировал постгрес

--- src/main/java/com/example/tea_app_test/model/user/Role.java
+++ src/main/java/com/example/tea_app_test/model/user/Role.java
@@ -4,11 +4,14 @@ package com.example.tea_app_test.model.user;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    ADMIN,
-    USER;
+    ROLE_ADMIN,
+    ROLE_MODERATOR,
+    ROLE_USER;
 
     @Override
     public String getAuthority() {
         return name();
     }
+
+
 }
